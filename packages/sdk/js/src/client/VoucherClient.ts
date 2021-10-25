2082883ddc028df22f24ecb2410fd836742e3f01
Xinyao(Alvin) Sun <lucklyric@gmail.com>
2021-10-25 03:25:10 -0600

    Feature: Front SDK to check/init collection (#20)
    * feat(js-sdk): Full config of client
    
    * feat(js-sdk): Add collection check and init collection
    
    * docs(js-sdk): Add readme for new apis

--- packages/sdk/js/src/client/VoucherClient.ts
+++ packages/sdk/js/src/client/VoucherClient.ts
@@ -1,5 +1,7 @@
 import * as fcl from "@onflow/fcl";
 import * as t from "@onflow/types";
+import {checkVoucherCollection} from "../cadence/check_voucher_collection";
+import {initVoucherCollection} from "../cadence/init_voucher_collection";
 import {getFLOWBalanceScript} from "../cadence/get_flow_balance";
 import {getFUSDBalanceScript} from "../cadence/get_fusd_balance";
 import {transferFLOWScript} from "../cadence/transfer_flow";
@@ -17,6 +19,8 @@ export interface VoucherClient {
     FUSDBalance(address: string): Promise<number>;
     transferFLOW(to: string, amount: string): Promise<string>;
     FLOWBalance(address: string): Promise<number>;
+    checkVoucherCollection(address: string): Promise<boolean>;
+    initVoucherCollection(): Promise<string>;
 }
 
 export class FclVoucherClient implements VoucherClient {
@@ -31,21 +35,25 @@ export class FclVoucherClient implements VoucherClient {
             case FlowEnv.flowTestnet: {
                 await fcl
                     .config()
-                    .put("accessNode.api", "http://localhost:8080")
-                    .put("discovery.wallet", "http://localhost:8701/fcl/authn")
+                    .put("accessNode.api", "https://access-testnet.onflow.org") // connect to Flow testnet
+                    .put("challenge.handshake", "https://flow-wallet-testnet.blocto.app/authn") // use Blocto testnet wallet
                     .put("0xFUNGIBLE_TOKEN_ADDRESS", "0x9a0766d93b6608b7")
                     .put("0xFUSD_ADDRESS", "0xe223d8a629e49c68")
-                    .put("0xFLOW_TOKEN_ADDRESS", "0x7e60df042a9c0868");
+                    .put("0xFLOW_TOKEN_ADDRESS", "0x7e60df042a9c0868")
+                    .put("0xVOUCHER_ADDRESS", "0xe2f1b000e0203c1d")
+                    .put("0xNON_FUNGIBLE_TOKEN_ADDRESS", "0x631e88ae7f1d7c20");
                 break;
             }
             case FlowEnv.flowMainnet: {
                 await fcl
                     .config()
-                    .put("accessNode.api", "http://localhost:8080")
-                    .put("discovery.wallet", "http://localhost:8701/fcl/authn")
+                    .put("accessNode.api", "https://flow-access-mainnet.portto.io")
+                    .put("challenge.handshake", "https://flow-wallet.blocto.app/authn") // use Blocto wallet
                     .put("0xFUNGIBLE_TOKEN_ADDRESS", "0xf233dcee88fe0abe")
                     .put("0xFUSD_ADDRESS", "0x3c5959b568896393")
-                    .put("0xFLOW_TOKEN_ADDRESS", "0x1654653399040a61");
+                    .put("0xFLOW_TOKEN_ADDRESS", "0x1654653399040a61")
+                    .put("0xVOUCHER_ADDRESS", "0x0d77ec47bbad8ef6")
+                    .put("0xNON_FUNGIBLE_TOKEN_ADDRESS", "0x1d7e57aa55817448");
                 break;
             }
             case FlowEnv.localEmulator:
@@ -56,7 +64,9 @@ export class FclVoucherClient implements VoucherClient {
                     .put("discovery.wallet", "http://localhost:8701/fcl/authn")
                     .put("0xFUNGIBLE_TOKEN_ADDRESS", "0xee82856bf20e2aa6")
                     .put("0xFUSD_ADDRESS", "0xf8d6e0586b0a20c7")
-                    .put("0xFLOW_TOKEN_ADDRESS", "0x0ae53cb6e3f42a79");
+                    .put("0xFLOW_TOKEN_ADDRESS", "0x0ae53cb6e3f42a79")
+                    .put("0xVOUCHER_ADDRESS", "0x01cf0e2f2f715450")
+                    .put("0xNON_FUNGIBLE_TOKEN_ADDRESS", "0xf8d6e0586b0a20c7");
         }
     }
 
@@ -156,7 +166,42 @@ export class FclVoucherClient implements VoucherClient {
             return fcl.decode(response);
         } catch (error) {
             console.error(error);
-            return Promise.reject("Something is wrong with fetching FUSD balance");
+            return Promise.reject("Something is wrong with fetching FLOW balance");
+        }
+    }
+
+
+    public async checkVoucherCollection(address: string): Promise<boolean> {
+        try {
+            const response = await fcl.send([
+                checkVoucherCollection,
+                fcl.args([fcl.arg(address, t.Address)]),
+                fcl.limit(1000)
+            ]);
+            return fcl.decode(response);
+        } catch (error) {
+            console.error(error);
+            return Promise.reject("Something is wrong with checking Voucher Collection");
+        }
+    }
+
+    public async initVoucherCollection(): Promise<string> {
+        try {
+            const response = await fcl.send([
+                initVoucherCollection,
+                fcl.proposer(fcl.currentUser().authorization),
+                fcl.authorizations([fcl.currentUser().authorization]),
+                fcl.limit(1000),
+                fcl.payer(fcl.currentUser().authorization)
+            ]);
+            const ret = await fcl.tx(response).onceSealed();
+            if (ret.errorMessage !== "" && ret.status != 4) {
+                return Promise.reject(ret.errorMessage);
+            }
+            return response.transactionId;
+        } catch (error) {
+            console.error(error);
+            return Promise.reject("Something is wrong with this transaction");
         }
     }
 }
