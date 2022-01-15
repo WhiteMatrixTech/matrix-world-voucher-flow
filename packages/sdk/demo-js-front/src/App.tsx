b9b3c7eeab17e1459d3e3de4b95a6cb1d55d5c6e
Xinyao(Alvin) Sun <lucklyric@gmail.com>
2022-01-14 17:18:50 -0800

    Feat/assets (#29)
    * feat(js-sdk): Add assets testent support
    
    * feat(sdk): Assets to JS SDK

--- packages/sdk/demo-js-front/src/App.tsx
+++ packages/sdk/demo-js-front/src/App.tsx
@@ -10,8 +10,9 @@ import {
 function App() {
   const client = new FclVoucherClient();
   const check = async () => {
-    await client.setupGlobalFcl(FlowEnv.localEmulator);
+    // await client.setupGlobalFcl(FlowEnv.localEmulator);
     // await client.setupGlobalFcl(FlowEnv.flowTestnet);
+    await client.setupGlobalFcl(FlowEnv.flowMainnet);
     await fcl.logIn();
     await fcl.authenticate();
   };
@@ -21,14 +22,21 @@ function App() {
     let ret;
     const user = await fcl.currentUser().snapshot();
     console.log(user)
-    ret = await client.checkVoucherCollection(user.addr);
+
+    ret = await client.checkAssetsCollection(user.addr);
     console.log(ret);
 
-    ret = await client.initVoucherCollection();
-    console.log(ret);
+    // ret = await client.initAssetsCollection();
+    // console.log(ret);
 
-    ret = await client.transferVoucher("0x01cf0e2f2f715450", 0);
-    console.log(ret);
+    // ret = await client.checkVoucherCollection(user.addr);
+    // console.log(ret);
+    //
+    // ret = await client.initVoucherCollection();
+    // console.log(ret);
+
+    // ret = await client.transferVoucher("0x01cf0e2f2f715450", 0);
+    // console.log(ret);
 
     // transferFUSD
     // console.log("FUSD test");
@@ -73,16 +81,16 @@ function App() {
     //     // Please reserve more FLOW in your wallet, it seems like will run out of storage and likely cause a failed mint
     // }
 
-    ret = await client.FLOWBalance("0x01cf0e2f2f715450");
-    console.log(ret);
-
-    ret = await client.transferFLOW("0x01cf0e2f2f715450", "11.1");
-    console.log(ret);
-
-    ret = await client.FLOWBalance(user.addr);
-    console.log(ret);
-    ret = await client.FLOWBalance("0x01cf0e2f2f715450");
-    console.log(ret);
+    // ret = await client.FLOWBalance("0x01cf0e2f2f715450");
+    // console.log(ret);
+    //
+    // ret = await client.transferFLOW("0x01cf0e2f2f715450", "11.1");
+    // console.log(ret);
+    //
+    // ret = await client.FLOWBalance(user.addr);
+    // console.log(ret);
+    // ret = await client.FLOWBalance("0x01cf0e2f2f715450");
+    // console.log(ret);
   };
 
   return (
---
+++ packages/sdk/js/src/cadence/check_assets_collection.ts
@@ -0,0 +1,10 @@
+import * as fcl from "@onflow/fcl";
+
+export const checkAssetsCollection: string = fcl.script`
+import NonFungibleToken from 0xNON_FUNGIBLE_TOKEN_ADDRESS
+import MatrixWorldAssetsNFT from 0xASSETS_ADDRESS
+
+pub fun main(addr: Address): Bool {
+    let ref = getAccount(addr).getCapability<&{NonFungibleToken.CollectionPublic}>(MatrixWorldAssetsNFT.collectionPublicPath).check()
+    return ref
+}`;
